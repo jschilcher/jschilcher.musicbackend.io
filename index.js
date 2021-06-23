d3f78b91cf52623c75e0650789f2c3050ebf362f
Jenelle Schilcher <jschilcher8@gmail.com>
2021-06-23 18:21:52 -0500

    Add app.get

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const app = express();
 app.listen(5000, function(){
     console.log("Server started. Listening on port 5000");
 });
+
+app.get("/api/songs/:id", (req, res) => {
+    const id = req.params.id;
+    const music = repoContext.songs.findSongById(id);
+    return res.send(music);
+})
