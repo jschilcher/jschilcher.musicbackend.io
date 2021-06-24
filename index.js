6f6bd51978b9cafd9fdc728c71a861f0a0dd2502
Jenelle Schilcher <jschilcher8@gmail.com>
2021-06-23 19:13:31 -0500

    Add post and middleware

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const repoContext = require("./repository/repository-wrapper");
+const {validateSong} = require("./middleware/song-validation");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.listen(5000, function(){
     console.log("Server started. Listening on port 5000");
+});
+
+app.get("/api/songs", (req, res) => {
+    const music = repoContext.songs.findAllSongs();
+    return res.send(music);
 });
 
 app.get("/api/songs/:id", (req, res) => {
     const id = req.params.id;
     const music = repoContext.songs.findSongById(id);
     return res.send(music);
+});
+
+app.post("/api/songs", (req, res) => {
+    const newSong = req.body;
+    const addSong = repoContext.songs.createSong(newSong);
+    return res.send(addSong);
 })
